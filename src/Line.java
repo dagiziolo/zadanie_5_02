ff831ff782151a18335bd3d1082763db9e8cfb6e
dagma <dagmara.ziolkowska@wp.pl>
2019-01-23 19:27:23 +0100

    Dodanie private oraz geterów i seterów do Klasy Line i LineMethod.

--- src/Line.java
+++ src/Line.java
@@ -1,9 +1,25 @@
 public class Line {
-    Point startPoint;
-    Point endPoint;
+    private Point startPoint;
+    private Point endPoint;
 
     public Line(Point startPoint, Point endPoint) {
         this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+
+    public Point getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(Point startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(Point endPoint) {
         this.endPoint = endPoint;
     }
 }
--- src/LineMethod.java
+++ src/LineMethod.java
@@ -1,8 +1,8 @@
 public class LineMethod {
 
     double length (Line line1) {
-        double xtmp = Math.pow(line1.endPoint.getX() - line1.startPoint.getX(),2);
-        double ytmp = Math.pow(line1.endPoint.getY() - line1.startPoint.getY(),2);
+        double xtmp = Math.pow(line1.getEndPoint().getX() - line1.getStartPoint().getX(),2);
+        double ytmp = Math.pow(line1.getEndPoint().getY() - line1.getStartPoint().getY(),2);
         double len = Math.pow(xtmp + ytmp,0.5);
         return len;
     }
